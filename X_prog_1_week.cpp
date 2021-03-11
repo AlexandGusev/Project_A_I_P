a527af5348626fd8c682754596ea0296d1bf3d84
Aleksander Gusev <wanna_be_fly@mail.ru>
2021-03-11 19:04:23 +0300

    Добавьте файлы проекта.

---
+++ X_prog_1_week.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+//Пользователь вводит число. Выведите на экран квадрат этого числа, куб этого числа.
+
+
+int main()
+{
+    float x;
+    std::cout << "Enter number: " << std::endl;
+    std::cin >> x;
+    std::cout << "x^2 = " << x * x << std::endl;
+    std::cout << "x^3 = " << x * x * x << std::endl;
+    std::getchar();
+    std::getchar();
+
+}
