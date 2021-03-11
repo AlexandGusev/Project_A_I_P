a4c7942218e8d00da94daf36e840629204a372e7
Aleksander Gusev <wanna_be_fly@mail.ru>
2021-03-11 19:10:49 +0300

    Добавьте файлы проекта.

---
+++ Letter_A_1_week.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+//Выводит первую букву моего имени
+{
+    std::cout << " ********* " << std::endl
+              << "**       **" << std::endl
+              << "**       **" << std::endl
+              << "***********" << std::endl
+              << "**       **" << std::endl
+              << "**       **" << std::endl
+              << "**       **" << std::endl;
+}
