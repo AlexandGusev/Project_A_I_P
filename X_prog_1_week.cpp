9dacdc9fd4a920f8e8ee6fab0f9a512e81171aaa
Aleksander Gusev <wanna_be_fly@mail.ru>
2021-03-11 22:16:31 +0300

    Work with file

--- X_prog_1_week.cpp
+++ X_prog_1_week.cpp
@@ -1,12 +1,31 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
 //Пользователь вводит число. Выведите на экран квадрат этого числа, куб этого числа.
 
 
 int main()
 {
     float x;
+
     std::cout << "Enter number: " << std::endl;
+
     std::cin >> x;
+
+    std::ofstream fo;
+    std::ifstream fi;
+
+    fo.open("in.txt");
+    fo << x ;
+    fo.close();
+
+    fi.open("in.txt");
+    fi >> x;
+    fi.close();
+
+    
+    
+   
     std::cout << "x^2 = " << x * x << std::endl;
     std::cout << "x^3 = " << x * x * x << std::endl;
     std::getchar();
