50e0b5c22bd150f19e0668ddbeaa30491c4e7bed
Aleksander Gusev <wanna_be_fly@mail.ru>
2021-03-11 18:49:52 +0300

    Добавьте файлы проекта.

---
+++ Formula_1_week.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include<cmath>
+
+int main()
+{
+    // решение формулы e^(x-2)+|sin(x)|-x^4*cos(1/x)
+    float x;
+    float ans;
+    std::cout << "This app help you with this formul:" << std::endl
+        << "e^(x-2)+|sin(x)|-x^4*cos(1/x)" << std::endl
+        << "Enter number x" << std::endl;
+    std::cin >> x;
+    ans = exp(x - 2) + abs(sin(x)) - x * x * x * x * cos(1 / x);
+    std::cout << "Answer is " << ans << std::endl;
+
+}
