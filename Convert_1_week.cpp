279b29157b9f329e634eb9c4132c7f8da55925ba
Aleksander Gusev <wanna_be_fly@mail.ru>
2021-03-12 17:07:28 +0300

    Work with files

--- Convert_1_week.cpp
+++ Convert_1_week.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <cmath>
+#include <fstream>
 
 //Сделать конвертер величин. Формат ввода: Направление_Перевода Величина.
 //Кулинария (чайная, столовая, кружка)
@@ -13,9 +14,24 @@ int main() {
     std::cout << "select translations direction:\n";
     for (int i = 0; i < 3; i++)
         std::cout << i + 1 << '.' << name[i] << '\t' << i + 1 << '.' << name[i] << std::endl;
+
+
     std::cin >> a >> b;
+    std::ofstream fo;
+    std::ifstream fi;
+
+    
     std::cout << "enter value: ";
     std::cin >> v;
+
+    fo.open("in.txt");
+    fo << v;
+    fo.close();
+
+    fi.open("in.txt");
+    fi >> v;
+    fi.close();
+
     k = pow(3, b - 1) + pow(3, a - 1);
     if (a == 3)
         k /= 3 * 12;
