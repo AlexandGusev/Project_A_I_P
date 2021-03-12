7a936d30bfff49c49c3e837a4a310ffba3ba08c9
Aleksander Gusev <wanna_be_fly@mail.ru>
2021-03-12 17:09:50 +0300

    Work with files

--- Formula_1_week.cpp
+++ Formula_1_week.cpp
@@ -1,15 +1,29 @@
 ﻿#include <iostream>
 #include<cmath>
+#include <fstream>
 
 int main()
 {
     // решение формулы e^(x-2)+|sin(x)|-x^4*cos(1/x)
     float x;
     float ans;
+
+    std::ofstream fo;
+    std::ifstream fi;
+
     std::cout << "This app help you with this formul:" << std::endl
         << "e^(x-2)+|sin(x)|-x^4*cos(1/x)" << std::endl
         << "Enter number x" << std::endl;
     std::cin >> x;
+
+    fo.open("in.txt");
+    fo << x;
+    fo.close();
+
+    fi.open("in.txt");
+    fi >> x;
+    fi.close();
+
     ans = exp(x - 2) + abs(sin(x)) - x * x * x * x * cos(1 / x);
     std::cout << "Answer is " << ans << std::endl;
 
