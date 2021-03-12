2bc1ef8b8d5bfbba301aedf3242b516a51850d16
Aleksander Gusev <wanna_be_fly@mail.ru>
2021-03-12 17:15:27 +0300

    Work with files

--- Rhombus_1_week.cpp
+++ Rhombus_1_week.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <iomanip>
 #include <math.h>
+#include <fstream>
 //Пользователь вводит параметры фигуры. Найти периметр и площадь ромба
 
 int main()
@@ -12,6 +13,17 @@ int main()
     std::cin >> d1;
     std::cout << "Enter the second diagonal:";
     std::cin >> d2;
+
+    std::ofstream fo;
+    std::ifstream fi;
+
+    fo.open("in.txt");
+    fo << d1 << " " << d2;
+    fo.close();
+
+    fi.open("in.txt");
+    fo << d1  << d2;
+    fi.close();
 
     float plo = (d1 * d2) / 2;
     float per = 4 * sqrt((d1 / 2) * (d1 / 2) + (d2 / 2) * (d2 / 2));
